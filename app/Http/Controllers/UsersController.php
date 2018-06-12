462687a08c7aad69d95d2b149472b03b90881c55
sara.kawamoto <sara.kawamoto@rakuten.com>
2018-06-12 07:20:30 +0000

    favorites

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,18 +9,7 @@ use App\Micropost; // add
 
 class UsersController extends Controller
 {
-    
-    public function store(Request $request, $id)
-    {
-        \Auth::user()->follow($id);
-        return redirect()->back();
-    }
-
-    public function destroy($id)
-    {
-        \Auth::user()->unfollow($id);
-        return redirect()->back();
-    }
+   
 
     public function index()
     {
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
 Route::group(['middleware' => ['auth']], function () {
    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+   Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
     Route::group(['prefix' => 'users/{id}'], function () {
         Route::post('follow', 'UserFollowController@store')->name('user.follow');
         Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
